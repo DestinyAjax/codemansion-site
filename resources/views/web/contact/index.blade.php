83b8f50f26afee25740903805ad30d20ed0063ad
Destiny Ajax <destinyajakaiye@gmail.com>
2018-01-16 12:23:38 -0800

    Implemented full setup

---
+++ resources/views/web/contact/index.blade.php
@@ -0,0 +1,7 @@
+@extends('web.partials.app')
+
+@section('content')
+<div class="flex-center position-ref full-height">
+    <h2>Contact Page</h2>
+</div>
+@endsection
---
+++ resources/views/web/solutions/index.blade.php
@@ -0,0 +1,7 @@
+@extends('web.partials.app')
+
+@section('content')
+<div class="flex-center position-ref full-height">
+    <h1>Solutions Page</h1>
+</div>
+@endsection
